c5efa416d606eabc1f39da6d04b9b4eb95fce5fd
terahje <terahjequinn@gmail.com>
2020-08-16 16:17:39 -0700

    add handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl)
+var formEl = document.querySelector("#task-form");
 
 var taskToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    
+    event.preventDefault();
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     taskToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
