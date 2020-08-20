621a8ca376df1ff54c2d3e72bc5925bfba229734
terahje <terahjequinn@gmail.com>
2020-08-19 21:26:01 -0700

    added save task to edit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskIdCounter = 0;
 
+var pageContentEl = document.querySelector("#page-content");
+
 
 var taskFormHandler = function(event) {
     
@@ -95,4 +97,42 @@ var createTaskEl = function(taskDataObj) {
     taskIdCounter++;
 }
 
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
+var editTask = function(taskId) {
+      var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // get content from tsk name and type
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type'").value = taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+    formEl.setAttribute("data-task-id", taskId);
+    
+};
+
+
 formEl.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function(event) {
+    // get target element from event
+    var targetEl = event.target;
+
+    // edit button was clicked
+    if(targetEl.matches(".edit-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+    // delete button was clicked
+    if(event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+pageContentEl.addEventListener("click", taskButtonHandler);
